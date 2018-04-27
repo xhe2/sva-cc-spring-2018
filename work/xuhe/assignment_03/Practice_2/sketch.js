721149ef03f5f129121c26cb438d3860c9f3d032
xhe2 <xhe2@sva.edu>
2018-04-27 18:55:10 -0400

    assignment_03

--- work/xuhe/assignment_03/Practice_2/sketch.js
+++ work/xuhe/assignment_03/Practice_2/sketch.js
@@ -16,9 +16,13 @@ function draw() {
   var y = amp * cos(angle);
 
   var secAngle = map(sec, 0, 60, 0, Math.PI * 2) - Math.PI / 2;
+  var minAngle = map(min, 0, 60, 0, Math.PI * 2) - Math.PI / 2;
 
   var secX = 100 * cos(secAngle);
   var secY = 100 * sin(secAngle);
+
+  var minX = 100 * cos(minAngle);
+  var minY = 100 * sin(minAngle);
 
   push();
   noStroke();
@@ -35,10 +39,11 @@ function draw() {
   noStroke();
 
   fill(250, 50);
-  ellipse(x/1.5, (-y)/1.5,220,220);
+  //ellipse(minX/1.4, (-minY)/1.4,230,230) why it doesn't work
+  ellipse(x/1.5, (-y)/1.5,230,230);
   fill(250);
   textSize(18);
-  text('\n' + sec, x, y-10);
+  //text('\n' + sec, x, y-10);
   //textSize(18);
   //text('\n' + min, x, y-60);
   pop();
@@ -48,8 +53,8 @@ function draw() {
   noStroke();
   fill(250,random(50,60));
   //ellipse(x/1.5,(-y)/1.5,80,80);
-  fill(255);
-  ellipse(secX, secY, 80, 80);
+  fill(255,140);
+  ellipse(secX/1.4, secY/1.4, 80, 80);
   pop();
 
 }
